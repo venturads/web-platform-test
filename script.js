f3567e7245bf57cfd431c9efc3ca99dee40f5000
venturads <albert.venturads@yahoo.com>
2021-09-09 17:10:00 -0700

    1 file modified

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ console.log('hello!');
 
 let ages = [22, 11, 33, 55];
 const Employees = new Map();
-Employees.set('Albert', 45, 'Engineer');
+
+Employees.set(45, 'Albert');
 
 for (let n = 0; n < ages.length; n++) {
   if (ages[n] > 20) {
@@ -11,4 +12,4 @@ for (let n = 0; n < ages.length; n++) {
   //console.log(ages[n]);
 }
 
-console.log(Employees.get('Albert'));
+console.log(Employees.get(45));
